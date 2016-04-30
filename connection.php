4a9a70834e8c66723e8bb498c5e52844f2b407c1
nnaemekaopara <1513552@rgu.ac.uk>
2016-04-30 11:29:35 +0100

    Initial commit

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_SERVER', 'us-cdbr-azure-west-c.cloudapp.net');
+define('DB_USERNAME', 'b30a36477e69a6');
+define('DB_PASSWORD', 'f1fb3a78');
+define('DB_DATABASE', 'Vulnerabl');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+?>
---
+++ submit.php
@@ -0,0 +1,29 @@
+<?php
+$msg = "";
+if(isset($_POST["submit"]))
+{
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $password = md5(mysqli_real_escape_string($db, $_POST['password']));
+
+
+
+    $sql="SELECT email FROM users WHERE email='$email'";
+    $result=mysqli_query($db,$sql);
+    $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
+    if(mysqli_num_rows($result) == 1)
+    {
+        $msg = "Sorry...This email already exists...";
+    }
+    else
+    {
+        //echo $name." ".$email." ".$password;
+        $query = mysqli_query($db, "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')")or die(mysqli_error($db));
+        if($query)
+        {
+            $msg = "Thank You! you are now registered. click <a href='index.php'>here</a> to login";
+        }
+
+    }
+}
+?>
